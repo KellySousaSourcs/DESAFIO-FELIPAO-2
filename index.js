b72e2f073c70587c4bcfc274730280db65451dd0
KellySousaSourcs <kellysousasourcs@gmail.com>
2025-03-09 22:59:49 -0300

    Projeto finalizado

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-console.log("ola")
-console.log("ola")
+function calculadoraRanqueada(derrotas, vitorias ){
+    let saldo = derrotas - vitorias;
+    let nivel;
+
+    if(vitorias < 10){
+        nivel = "Ferro";
+
+    }else if(vitorias >= 11 && vitorias <= 20){
+        nivel = "Bronze";
+
+    }else if(vitorias >= 21 && vitorias <= 50){
+        nivel = "Prata";
+
+    }else if(vitorias >= 51 && vitorias <= 80){
+        nivel = "Ouro";
+
+    }else if(vitorias >= 81 && vitorias <= 90){
+        nivel = "Diamante";
+
+    }else{
+        nivel = "Imortal";
+    }
+    
+    console.log(`O herói tem um saldo de ${saldo} e está no nivel de ${nivel}`);
+
+}
+
+    calculadoraRanqueada(110, 100);
